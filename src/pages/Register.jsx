ee384bb554f5e72278c322ff6eff809ca8fa2bde
Achsmboh <maulana.achsanxiipa1@gmail.com>
2022-10-20 15:53:51 +0700

    mengubah register

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,12 +1,61 @@
 import React from "react";
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+
 import { Form, Button } from "react-bootstrap";
 import "../styles/Register.css";
-import Image from 'react-bootstrap/Image';
+import Image from "react-bootstrap/Image";
 import { useTitle } from "utils/redux/UseTitle";
+import CustomButton from "components/CostumButton";
 
+function Register() {
+  const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPasword] = useState("");
+  const [name, setName] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [disabled, setDisabled] = useState(true);
 
-const Register = () => {
   useTitle("Register | Altagram");
+
+  useEffect(() => {
+    if (email && name && password) {
+      setDisabled(false);
+    } else {
+      setDisabled(true);
+    }
+  }, [name, email, password]);
+
+  const handleSubmit = async (e) => {
+    setLoading(true);
+    e.preventDefault();
+    const body = {
+      name,
+      email,
+      password,
+    };
+
+    axios
+      .post(`http://13.212.207.162:8000/register`, body)
+      .then((res) => {
+        console.log(res);
+        const { message, data } = res.data;
+        if (data) {
+          navigate("/");
+        }
+        alert(message);
+      })
+
+      .catch((err) => {
+        const { message } = err.response.data;
+        alert(message);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  };
+
   return (
     <div className="container-fluid">
       <div className="row">
@@ -14,55 +63,45 @@ const Register = () => {
           <p className="text-right mt-2">
             Have an account? <a href="/login">Login</a>
           </p>
-          <Image src="https://o.remove.bg/downloads/0bfc7f12-42f7-4b60-9108-1d0490d64a5d/3683-removebg-preview.png" className="images row"></Image>
+          <Image src="https://o.remove.bg/downloads/0bfc7f12-42f7-4b60-9108-1d0490d64a5d/3683-removebg-preview.png" className="w-100"></Image>
         </div>
         <div className="coloo col-8">
           <div className="border-form d-flex flex-column justify-content-center h-custom-2 w-75 pt-4">
             <h1 className="sign text-center">Sign In</h1>
-            <Form className="form-register">
+            <form className="form-register" onSubmit={(e) => handleSubmit(e)}>
               <Form.Group className="mb-3" controlId="formBasicEmail">
                 <Form.Label>Name</Form.Label>
-                <Form.Control className="box" type="name" placeholder="Your Name" />
+                <Form.Control className="box" type="name" placeholder="Your Name" onChange={(e) => setName(e.target.value)} />
               </Form.Group>
 
               <Form.Group className="mb-3" controlId="formBasicEmail">
                 <Form.Label>Email address</Form.Label>
-                <Form.Control className="box" type="email" placeholder="Your Email" />
+                <Form.Control className="box" type="email" placeholder="Your Email" onChange={(e) => setEmail(e.target.value)} />
               </Form.Group>
 
               <Form.Group className="mb-3" controlId="formBasicPassword">
                 <Form.Label>Password</Form.Label>
-                <Form.Control className="box" type="password" placeholder="Password" />
+                <Form.Control className="box" type="password" placeholder="Password" onChange={(e) => setPasword(e.target.value)} />
               </Form.Group>
-              <Form.Group
-                className="mb-3"
-                controlId="formBasicCheckbox"
-              ></Form.Group>
-            
-            <div className="form-check text-right">
-              <input
-                className="form-check-input text-right"
-                type="checkbox"
-                value=""
-                id="flexCheckDefault"
-              />
-              <label className="form-desc text-right" for="flexCheckDefault">
-                I ve read and agree with{" "}
-                <a href="#!"> Terms Of Service</a>{" "}
-                and our{" "}<a href="#!">privacy policy</a>
-              </label>
-            </div>
-            <Form.Group className="button-sign">
-            <Button className="submit text-black fw-bold" size="lg">
-              Sign Up
-            </Button>
-            </Form.Group>
-            </Form>
+              <Form.Group className="mb-3" controlId="formBasicCheckbox"></Form.Group>
+
+              <div className="form-check text-right">
+                <input className="form-check-input text-right" type="checkbox" value="" id="flexCheckDefault" />
+                <label className="form-desc text-right" for="flexCheckDefault">
+                  I ve read and agree with <a href="#!"> Terms Of Service</a> and our <a href="#!">privacy policy</a>
+                </label>
+              </div>
+
+              <CustomButton id="btn-register" label="Register" loading={loading || disabled} />
+              {/* <Button className="submit text-black fw-bold w-100" size="lg" onClick={(e) => handleSubmit(e)} loading={loading || disabled}>
+                  Sign Up
+                </Button> */}
+            </form>
           </div>
         </div>
       </div>
     </div>
   );
-};
+}
 
 export default Register;
